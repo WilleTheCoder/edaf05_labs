042458ae515f523f150ac47af208e50e394da71e
wille <wille-hif@hotmail.com>
2021-04-29 12:58:20 +0200

    small change

--- src/lab3/makingfriends.java
+++ src/lab3/makingfriends.java
@@ -3,16 +3,11 @@ package lab3;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class makingfriends {
 
